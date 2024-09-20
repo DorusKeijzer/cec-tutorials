cc545471be40c814874b2521ac33155065214850
Dorus <doruskeijzer@gmail.com>
2024-09-20 07:42:49 +0200

    updated docker run.sh

--- docker/assignment/run.sh
+++ docker/assignment/run.sh
@@ -4,4 +4,7 @@ docker run \
     --rm -d \
     --name notifications-service \
     -p 3000:3000 \
-    dclandau/cec-notifications-service "$@"
+    dclandau/cec-notifications-service \
+    --secret-key QJUHsPhnA0eiqHuJqsPgzhDozYO4f1zh \
+    --external-ip localhost
+
